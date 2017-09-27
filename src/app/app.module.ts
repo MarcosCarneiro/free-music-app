3dba34437bbdc75a225d87e87ecca562fc6c0759
Marcos Carneiro Lima <marcos@prolins.com.br>
2017-09-27 18:21:35 -0300

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from "@angular/forms";
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
